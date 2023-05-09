94989dd9349bd0f14283272946833e224912e531
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-09 10:31:40 -0600

    complete challenge 1-3

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here...
+function initialize(){
+    fetch('http://localhost:3000/menu')
+    .then(r=>r.json())
+    .then(menu=>{menu.forEach((item)=>{
+        let span = document.createElement('span');
+        const menuItems = document.querySelector('#menu-items')
+        span.textContent = item.name;
+        menuItems.appendChild(span);
+        span.addEventListener('click', ()=>{
+            displayDish(item);
+        })
+    }) 
+    displayDish(menu[0])
+    })
+}
+
+function displayDish(dish){
+    const dishView = document.querySelector('#dish')
+    dishView.querySelector('#dish-image').src = dish.image;
+    dishView.querySelector('#dish-text').querySelector('#dish-name').textContent = dish.name;
+    dishView.querySelector('#dish-text').querySelector('#dish-description').textContent = dish.description;
+    dishView.querySelector('#dish-text').querySelector('#dish-price').textContent = dish.price;
+}
+initialize();
